801fe90198c25a37492354cee5a3a6795aef5011
Sam <reply2samuel@gmail.com>
2020-06-23 16:10:47 -0400

    add required packages for material ui example

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Dashboard from "./Dashboard/Dashboard";
 
 ReactDOM.render(
-    App(),
+    <Dashboard/>,
   document.getElementById('root')
 );
 
