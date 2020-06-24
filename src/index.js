05927a599ae11d29ea52ff1e37bdaeb9735e2ced
Kaitlyn Chan <ununoctium829@gmail.com>
2020-06-24 16:01:49 -0700

    ahhh

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Dashboard from './Dashboard/Dashboard';
 
-<<<<<<< HEAD
-ReactDOM.render(
-  App(),
-  document.getElementById('root')
-);
-=======
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
->>>>>>> 7192b56d4faa1358f406e904fdf08f329169094d
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
