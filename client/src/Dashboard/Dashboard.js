2b34e30e85f3092747e58d4e759846a11d67d1e0
Kaitlyn Chan <ununoctium829@gmail.com>
2020-07-01 16:35:31 -0700

    merge conflict

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-<<<<<<< HEAD
-import Reports from './Reports';
-import AppBarWithDrawer from './AppBarWithDrawer';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import QueryResults from './QueryResults';
-
-import { Styles } from './Styles'; //this must be the last import because it consumes the styles of all the above imports
-=======
 import Reports from './PageComponents/ReportsDashboard/ReportsDashboard';
 import AppBarWithDrawer from './AppBarWithDrawer/AppBarWithDrawer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -17,7 +9,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { PageRouter } from './PageComponents/PageRouter';
 import { stylesSettings } from './Utilities/Styles';
 const Styles = makeStyles(stylesSettings);
->>>>>>> ed2397bae0d27dbe52e303975a27c82028025648
 
 export default function Dashboard() {
   const classes = Styles();
@@ -29,18 +20,7 @@ export default function Dashboard() {
         <main className={classes.content}>
           <div className={classes.appBarSpacer} />
           <Container maxWidth="lg" className={classes.container}>
-<<<<<<< HEAD
-            <Switch>
-              <Route path={'/Dashboard'}>
-                <Reports classes={classes} />
-              </Route>
-              <Route path={'/Queries'}>
-                <QueryResults />
-              </Route>
-            </Switch>
-=======
             <PageRouter />
->>>>>>> ed2397bae0d27dbe52e303975a27c82028025648
           </Container>
         </main>
       </BrowserRouter>
