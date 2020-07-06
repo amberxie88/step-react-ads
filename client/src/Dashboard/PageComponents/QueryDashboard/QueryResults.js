dd18526a2cf0878c33fe560321142cab67fbafb6
Kaitlyn Chan <ununoctium829@gmail.com>
2020-07-06 09:32:59 -0700

    front end refactor

--- client/src/Dashboard/PageComponents/QueryDashboard/QueryResults.js
+++ client/src/Dashboard/PageComponents/QueryDashboard/QueryResults.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from './Title';
+import Title from '../../Utilities/Title';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
