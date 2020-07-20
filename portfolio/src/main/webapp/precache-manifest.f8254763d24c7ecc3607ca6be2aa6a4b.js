68913b461e01da83d2fdaa2a95dda7d61180b6ac
Kaitlyn Chan <ununoctium829@gmail.com>
2020-07-20 16:51:22 -0700

    hm

---
+++ portfolio/src/main/webapp/precache-manifest.f8254763d24c7ecc3607ca6be2aa6a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da64e098c8ded0610fd915565dafd29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33289ed0f8871ae19b68",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d0c673ac034bdc3b996",
+    "url": "/static/js/2.954fcb9a.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.954fcb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33289ed0f8871ae19b68",
+    "url": "/static/js/main.193677f6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
