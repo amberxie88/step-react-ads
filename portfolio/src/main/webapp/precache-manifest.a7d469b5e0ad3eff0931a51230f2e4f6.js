0c00579a7e006f6d8724002d8999212fb203c220
Kaitlyn Chan <ununoctium829@gmail.com>
2020-07-21 16:58:54 -0700

    removed session conflicts for state token, added logout functionality

--- portfolio/src/main/webapp/precache-manifest.a7d469b5e0ad3eff0931a51230f2e4f6.js
+++ portfolio/src/main/webapp/precache-manifest.a7d469b5e0ad3eff0931a51230f2e4f6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5da64e098c8ded0610fd915565dafd29",
+    "revision": "d6ff893cde9605ee9efa4ebbaf2391ce",
     "url": "/index.html"
   },
   {
-    "revision": "33289ed0f8871ae19b68",
+    "revision": "5f25d3a3b87fdf8c050d",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "8d0c673ac034bdc3b996",
-    "url": "/static/js/2.954fcb9a.chunk.js"
+    "revision": "0d0f43dcc50e0de6700c",
+    "url": "/static/js/2.f4e4f196.chunk.js"
   },
   {
     "revision": "b815bed3bedb5a8af7470bb094451139",
-    "url": "/static/js/2.954fcb9a.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f4e4f196.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "33289ed0f8871ae19b68",
-    "url": "/static/js/main.193677f6.chunk.js"
+    "revision": "5f25d3a3b87fdf8c050d",
+    "url": "/static/js/main.5d3bc9c2.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
