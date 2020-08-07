031bc8a5cb5d95efadbdcf493ec8bcdd3da57521
Kaitlyn Chan <ununoctium829@gmail.com>
2020-08-07 14:22:12 -0700

    mvn package folder

---
+++ server/target/server-1/precache-manifest.dde19b25a14ac8beac90b4bfe881799d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5066b469aa71c77ea9e72f94e4ca9cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8e7b01664d1438662d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b153b91d5ec0909ea88",
+    "url": "/static/js/2.6ae160da.chunk.js"
+  },
+  {
+    "revision": "5df52a1cfb5fbd252881b482c4dd93f8",
+    "url": "/static/js/2.6ae160da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8e7b01664d1438662d",
+    "url": "/static/js/main.8e82bdb6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
